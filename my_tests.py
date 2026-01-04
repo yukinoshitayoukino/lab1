f7de47da741c18d4b724005e0c5ae03d5bdf869b
kelturax <kelturax@gmail.com>
2026-01-03 22:53:57 -0800

    add file my_tests.py

--- my_tests.py
+++ my_tests.py
@@ -1,3 +1,12 @@
 import fastlab
 def test1():
     assert  fastlab.sum_two_args(2,2) == 4
+
+def test2():
+    assert  fastlab.sum_two_args(2.0001,2) == 4
+
+def test3():
+    assert fastlab.sum_two_args(3, 2) == 5
+
+def test4():
+        assert fastlab.sum_two_args("2", 2) == 4
