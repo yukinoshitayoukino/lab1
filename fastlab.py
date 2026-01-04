dd9354d830f2a9c983e1c9ca0613fb9173e6696a
kelturax <kelturax@gmail.com>
2026-01-03 22:50:54 -0800

    add file my_tests.py

--- fastlab.py
+++ fastlab.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
+def sum_two_args(x,y):
+    return x+y
 # Hello World route
 @app.get("/")
 def read_root():
---
+++ my_tests.py
@@ -0,0 +1,3 @@
+import fastlab
+def test1():
+    assert  fastlab.sum_two_args(2,2) == 4
