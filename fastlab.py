a879d035df09288c1f4f3d22d02e7c3a56a21b63
kelturax <kelturax@gmail.com>
2026-01-08 05:29:36 -0800

    add file some, styles

--- fastlab.py
+++ fastlab.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
 def sum_two_args(x,y):
     return x+y
@@ -7,3 +9,9 @@ def sum_two_args(x,y):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+@app.get("/some_url/{something}", response_class=HTMLResponse)
+async def read_something(request: Request, something: str):
+    return templates.TemplateResponse("some.html", {"request": request,
+"something": something})
